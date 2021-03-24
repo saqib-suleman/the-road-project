60b72ca53753a0ef8e42430f435930741dd99b86
Saqib Suleman <saqibs3291@gmail.com>
2021-03-24 20:04:58 +0500

    add navbar styles and animation

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+document.querySelectorAll(".nav-list a").forEach((item) => {
+  item.addEventListener("click", () => {
+    container.classList.remove("change");
+  });
+});
+
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+let i = 0;
+
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
 document.querySelectorAll(".navigation-button").forEach((item) => {
   item.addEventListener("click", () => {
     item.parentElement.parentElement.classList.toggle("change");
